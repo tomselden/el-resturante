4943bb586bccb648f97f78e339623be48f2c033e
Thomas Selden <thomasselden42@gmail.com>
2022-01-14 18:27:07 -0500

    added rants

--- controllers/places_controller.js
+++ controllers/places_controller.js
@@ -42,7 +42,9 @@ router.get("/new", (req, res) => {
 
 router.get("/:id", (req, res) => {
   db.Place.findById(req.params.id)
+    .populate("comments")
     .then((place) => {
+      console.log(place.comments);
       res.render("places/show", { place });
     })
     .catch((err) => {
@@ -63,8 +65,24 @@ router.get("/:id/edit", (req, res) => {
   res.send("GET edit form stub");
 });
 
-router.post("/:id/rant", (req, res) => {
-  res.send("GET /places/:id/rant stub");
+router.post("/:id/comment", (req, res) => {
+  console.log(req.body);
+  db.Place.findById(req.params.id)
+    .then((place) => {
+      db.Comment.create(req.body)
+        .then((comment) => {
+          place.comments.push(comment.id);
+          place.save().then(() => {
+            res.redirect(`/places/${req.params.id}`);
+          });
+        })
+        .catch((err) => {
+          res.render("error404");
+        });
+    })
+    .catch((err) => {
+      res.render("error404");
+    });
 });
 
 router.delete("/:id/rant/:rantId", (req, res) => {
--- views/places/show.jsx
+++ views/places/show.jsx
@@ -2,6 +2,25 @@ const React = require("react");
 const Def = require("../default");
 
 function show(data) {
+  let comments = (
+   <h3 className="inactive">
+     No comments yet!
+     </h3>
+  )
+  if (data.place.comments.length) {
+    comments = data.place.comments.map((c) => {
+      return (
+        <div className="border">
+          <h2 className="rant">{c.rant ? "Rant! 😡" : "Rave! 😻"}</h2>
+          <h4>{c.content}</h4>
+          <h3>
+            <stong>- {c.author}</stong>
+          </h3>
+          <h4>Rating: {c.stars}</h4>
+        </div>
+      );
+    });
+  }
   return (
     <Def>
       <main>
@@ -16,15 +35,9 @@ function show(data) {
             <h1>{data.place.name}</h1>
 
             <br />
-            <h2>
-              Description
-              </h2>
-            <h3>
-              {data.place.showEstablished()}
-              </h3>
-            <h4>
-              Serving {data.place.cuisines}
-            </h4>
+            <h2>Description</h2>
+            <h3>{data.place.showEstablished()}</h3>
+            <h4>Serving {data.place.cuisines}</h4>
             <br />
             <a href={`/places/${data.index}/edit`} className="btn btn-warning">
               Edit
