03e0771214ac60f2c059e137af406c493dabea06
Thomas Selden <thomasselden42@gmail.com>
2022-01-19 15:41:24 -0500

    part 10 RESTrant

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -7,6 +7,25 @@ function show(data) {
      No comments yet!
      </h3>
   )
+  let rating = (
+    <h3 className="inactive">
+      No yet rated!
+      </h3>
+   )
+  if (data.place.comments.length) {
+  let sumRatings = data.place.comments.reduce((tot, c) => {
+    return tot + c.stars
+  }, 0)
+  let averageRating = Math.round(sumRatings / data.place.comments.length)
+  let stars = ''
+  for (let i = 0; i < averageRating; i++) {
+    stars += '⭐️'
+  } 
+  rating = (
+    <h3>
+      {stars} Stars!
+    </h3>
+  )  
   if (data.place.comments.length) {
     comments = data.place.comments.map((c) => {
       return (
